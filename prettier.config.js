2e79aa0945378c91c346b1582c9cf739abd5b006
George Bockari <bit@revisioned.consulting>
2025-04-30 13:23:49 -0400

    chore: add pnpm lockfile and update package manager version in package.json

--- prettier.config.js
+++ prettier.config.js
@@ -3,5 +3,5 @@ module.exports = {
   singleQuote: true,
   semi: false,
   plugins: ['prettier-plugin-tailwindcss'],
-  tailwindStylesheet: './src/styles/tailwind.css',
+  tailwindStylesheet: './styles/tailwind.css',
 }
